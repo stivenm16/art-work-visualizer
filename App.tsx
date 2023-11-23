9f4cb32751d86244451bfffe92ce78d4ae827dc2
Kevin <stiivenmendez16@gmail.com>
2023-11-23 16:01:42 -0500

    app structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { config } from '@gluestack-ui/config' // Optional if you want to use default theme
+import {
+  Box,
+  GluestackUIProvider,
+  Progress,
+  ProgressFilledTrack,
+  Text,
+} from '@gluestack-ui/themed'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GluestackUIProvider config={config}>
+      <Box
+        width="100%"
+        justifyContent="center"
+        alignItems="center"
+        height={'$full'}
+        backgroundColor="lightblue"
+      >
+        <Text>Open up App.js to start working on your app!</Text>
+        <Progress value={55} w="$80" h="$1">
+          <ProgressFilledTrack h="$1" />
+        </Progress>
+      </Box>
+    </GluestackUIProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
